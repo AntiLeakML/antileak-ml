618900dc65be140901e2f5d3648720baaa7abd5d
Nat2194 <nathan.carlier@efrei.net>
2024-11-13 21:35:14 +0100

    example qui montre qu'on peut scanner les fichiers et repérer des patterns : ici chercher pandas et numpy dans les scripts python et jupyter notebook

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,119 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Fonction appelée lors de l'activation de l'extension
 export function activate(context: vscode.ExtensionContext) {
-  let test = 1;
+  console.log("L’extension 'decorator sample' est activée!");
 
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "antileak-ml" is now active!');
+  let timeout: NodeJS.Timeout | undefined = undefined;
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "antileak-ml.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from AntiLeak ML!");
+  // Définir un style de décoration pour "pandas"
+  const pandasDecorationType = vscode.window.createTextEditorDecorationType({
+    color: "green",
+    fontWeight: "bold",
+    borderWidth: "1px",
+    borderStyle: "solid",
+    borderColor: "darkgreen",
+  });
+
+  // Définir un style de décoration pour "numpy"
+  const numpyDecorationType = vscode.window.createTextEditorDecorationType({
+    color: "purple",
+    fontWeight: "bold",
+    borderWidth: "1px",
+    borderStyle: "solid",
+    borderColor: "darkpurple",
+  });
+
+  let activeEditor = vscode.window.activeTextEditor;
+
+  // Fonction pour mettre à jour les décorations dans le document actif
+  function updateDecorations() {
+    if (!activeEditor) {
+      return;
     }
+
+    // Vérifier si le fichier est de type Python ou Jupyter Notebook
+    const fileExtension = activeEditor.document.fileName.split(".").pop();
+    if (fileExtension !== "py" && fileExtension !== "ipynb") {
+      return;
+    }
+
+    const text = activeEditor.document.getText();
+    const pandasMatches: vscode.DecorationOptions[] = [];
+    const numpyMatches: vscode.DecorationOptions[] = [];
+
+    // Rechercher les occurrences de "pandas"
+    const pandasRegex = /\bpandas\b/gi;
+    let match;
+    while ((match = pandasRegex.exec(text)) !== null) {
+      const startPos = activeEditor.document.positionAt(match.index);
+      const endPos = activeEditor.document.positionAt(
+        match.index + match[0].length
+      );
+      const decoration = {
+        range: new vscode.Range(startPos, endPos),
+        hoverMessage: "Librairie **pandas** détectée.",
+      };
+      pandasMatches.push(decoration);
+    }
+
+    // Rechercher les occurrences de "numpy"
+    const numpyRegex = /\bnumpy\b/gi;
+    while ((match = numpyRegex.exec(text)) !== null) {
+      const startPos = activeEditor.document.positionAt(match.index);
+      const endPos = activeEditor.document.positionAt(
+        match.index + match[0].length
+      );
+      const decoration = {
+        range: new vscode.Range(startPos, endPos),
+        hoverMessage: "Librairie **numpy** détectée.",
+      };
+      numpyMatches.push(decoration);
+    }
+
+    // Appliquer les décorations pour "pandas" et "numpy"
+    activeEditor.setDecorations(pandasDecorationType, pandasMatches);
+    activeEditor.setDecorations(numpyDecorationType, numpyMatches);
+  }
+
+  // Fonction pour déclencher les mises à jour des décorations avec un délai
+  function triggerUpdateDecorations(throttle = false) {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = undefined;
+    }
+    if (throttle) {
+      timeout = setTimeout(updateDecorations, 500);
+    } else {
+      updateDecorations();
+    }
+  }
+
+  // Appliquer les décorations lors de l'activation de l'éditeur de texte
+  if (activeEditor) {
+    triggerUpdateDecorations();
+  }
+
+  // Mettre à jour les décorations lorsque l'éditeur actif change
+  vscode.window.onDidChangeActiveTextEditor(
+    (editor) => {
+      activeEditor = editor;
+      if (editor) {
+        triggerUpdateDecorations();
+      }
+    },
+    null,
+    context.subscriptions
   );
 
-  context.subscriptions.push(disposable);
+  // Mettre à jour les décorations lorsque le document est modifié
+  vscode.workspace.onDidChangeTextDocument(
+    (event) => {
+      if (activeEditor && event.document === activeEditor.document) {
+        triggerUpdateDecorations(true);
+      }
+    },
+    null,
+    context.subscriptions
+  );
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
